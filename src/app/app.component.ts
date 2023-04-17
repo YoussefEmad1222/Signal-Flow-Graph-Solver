0c4781d871b22a8fea04f966d1ebf74aecf36900
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-04-17 17:28:23 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,10 +21,33 @@ export class AppComponent {
       height: innerHeight,
       gridSize: 1,
       model: this.graph,
+      defaultLink: new joint.shapes.standard.Link(),
+      linkPinning: false,
+      validateConnection: function (
+        cellViewS: any,
+        magnetS: any,
+        cellViewT: any,
+        magnetT: any,
+        end: any,
+        linkView: any
+      ) {
+        // Prevent linking from input ports.
+        if (magnetS && magnetS.getAttribute('port-group') === 'in')
+          return false;
+        // Prevent linking from output ports to input ports within one element.
+        if (
+          cellViewS === cellViewT &&
+          magnetS.getAttribute('port-group') === 'out' &&
+          magnetT.getAttribute('port-group') === 'in'
+        )
+          return false;
+        // Prevent linking to input ports.
+        return magnetT && magnetT.getAttribute('port-group') === 'in';
+      },
     });
     let node = new joint.shapes.standard.Circle();
     node.position(100, 30);
-    node.resize(50, 50);
+    node.resize(75, 75);
     node.attr({
       body: {
         fill: 'orange',
@@ -36,10 +59,9 @@ export class AppComponent {
         fill: 'white',
       },
     });
-
     let node2 = new joint.shapes.standard.Circle();
     node2.position(innerWidth - 400, 30);
-    node2.resize(50, 50);
+    node2.resize(75, 75);
     node2.attr({
       body: {
         fill: 'green',
@@ -51,8 +73,66 @@ export class AppComponent {
         fill: 'white',
       },
     });
-    this.graph.addCell(node);
-    this.graph.addCell(node2);
+
+    var port1 = {
+      label: {
+        position: {
+          name: 'left',
+        },
+        markup: [
+          {
+            tagName: 'text',
+            selector: 'label',
+          },
+        ],
+      },
+      attrs: {
+        portBody: {
+          magnet: true,
+          r: 10,
+          fill: '#023047',
+          stroke: '#023047',
+        },
+      },
+      markup: [
+        {
+          tagName: 'circle',
+          selector: 'portBody',
+        },
+      ],
+    };
+    var port2 = {
+      label: {
+        position: {
+          name: 'right',
+        },
+        markup: [
+          {
+            tagName: 'text',
+            selector: 'label',
+          },
+        ],
+      },
+      attrs: {
+        portBody: {
+          magnet: true,
+          r: 10,
+          fill: '#023047',
+          stroke: '#023047',
+        },
+      },
+      markup: [
+        {
+          tagName: 'circle',
+          selector: 'portBody',
+        },
+      ],
+    };
+    node.addPort(port1);
+    node2.addPort(port2);
+    node.addTo(this.graph);
+    node2.addTo(this.graph);
+
     let toolview = new joint.dia.ToolsView({
       tools: [new joint.elementTools.Remove()],
     });
@@ -69,8 +149,14 @@ export class AppComponent {
     });
     paper.on('element:pointerclick', function (elementView: any) {
       console.log(elementView.model.attributes.attrs.label.text);
+      console.log(elementView.model.attributes.position);
     });
-    
+    paper.on('link:mouseenter', function (linkView: any) {
+      linkView.showTools();
+    });
+    paper.on('link:mouseleave', function (linkView: any) {
+      linkView.hideTools();
+    });
   }
   addNode() {
     this.nodeCr = '';
