5265c4348ffc652fe058aa34a23af55e10f354aa
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-04-17 16:53:03 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import * as jQuery from 'jquery';
-import * as _ from 'lodash';
-import * as $ from 'backbone';
 //import * as joint from 'jointjs';
 const joint = require('jointjs/dist/joint.js');
 
@@ -15,32 +12,67 @@ export class AppComponent {
   public start: string = '';
   public end: string = '';
   public graph: any;
-  ngOnit() {
-    this.graph = new joint.dia.Graph();
+  ngOnInit() {
+    let namespace = joint.shapes;
+    this.graph = new joint.dia.Graph({}, { cellNamespace: namespace });
     let paper = new joint.dia.Paper({
-      el: jQuery('#draw'),
-      width: innerWidth + 100,
-      height: innerHeight + 100,
-      gridSize: 10,
+      el: document.getElementById('draw'),
+      width: innerWidth,
+      height: innerHeight,
+      gridSize: 1,
       model: this.graph,
     });
-    let node = new joint.shapes.basic.Rect({
-      position: { x: 100, y: 30 },
-      size: { width: 100, height: 30 },
-      attrs: {
-        rect: { fill: 'blue' },
-        text: { text: 'my box', fill: 'white' },
+    let node = new joint.shapes.standard.Circle();
+    node.position(100, 30);
+    node.resize(50, 50);
+    node.attr({
+      body: {
+        fill: 'orange',
+        stroke: 'black',
+        strokeWidth: 2,
+      },
+      label: {
+        text: 'R(s)',
+        fill: 'white',
+      },
+    });
+
+    let node2 = new joint.shapes.standard.Circle();
+    node2.position(innerWidth - 400, 30);
+    node2.resize(50, 50);
+    node2.attr({
+      body: {
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 2,
+      },
+      label: {
+        text: 'C(s)',
+        fill: 'white',
       },
     });
     this.graph.addCell(node);
-    let node2 = node.clone();
-    node2.translate(300);
     this.graph.addCell(node2);
-    let link = new joint.dia.Link({
-      source: { id: node.id },
-      target: { id: node2.id },
+    let boundary = new joint.elementTools.Boundary();
+    let remove = new joint.elementTools.Remove();
+    let toolview = new joint.dia.ToolsView({
+      tools: [
+        new joint.elementTools.Remove(),
+      ],
     });
-    this.graph.addCells([node, node2, link]);
+    let toolview2 = new joint.dia.ToolsView({
+      tools: [
+        new joint.elementTools.Remove(),
+      ],
+    });
+    node.findView(paper).addTools(toolview);
+    node2.findView(paper).addTools(toolview2);
+    paper.on('element:mouseenter', function (elementView: any) {
+      elementView.showTools();
+    });
+    paper.on('element:mouseleave', function (elementView: any) {
+      elementView.hideTools();
+    });
   }
 
   addNode() {
