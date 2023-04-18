a48a2a8e2685d79c3f9d2b12233a1c383fa74410
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-04-18 07:47:14 +0200

    update

--- src/app/routh/routh.component.ts
+++ src/app/routh/routh.component.ts
@@ -3,21 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-routh',
   templateUrl: './routh.component.html',
-  styleUrls: ['./routh.component.css']
+  styleUrls: ['./routh.component.css'],
 })
 export class RouthComponent {
-numInputs: any;
-values:number[]=[];
-entered:boolean=false;
-
-takeinput(value:any) {
-  this.numInputs = value;
-  console.log("generated");
-  console.log(this.numInputs);
-  for (let i = 0; i < this.numInputs; i++) {
-    this.values.push(i);
+  numInputs: any;
+  values: number[] = [];
+  entered: boolean = false;
+  table: any;
+  takeinput(value: any) {
+    if (this.entered) {
+      this.values = [];
+      this.numInputs = 0;
+      this.entered = false;
+    }
+    if (value == '' || value == null || value == undefined) {
+      return;
+    }
+    this.numInputs = +value;
+    this.numInputs += 1;
+    console.log('generated');
+    console.log(this.numInputs);
+    let number = new Array(this.numInputs);
+    this.values = number;
+    for (let i = 0; i < this.numInputs; i++) {
+      number[i] = i;
+    }
+    this.entered = true;
   }
-  this.entered=true;
-}
-
+  takeInput(value: any,index: any) {
+   this.values[index] = +value;
+  }
+   solve() {
+    console.log(this.values);
+    this.table = new Array(this.numInputs);
+    for (let i = 0; i < this.numInputs; i++) {
+      this.table[i] = new Array(3);
+      this.table[i].fill(0);
+    }
+  }
 }
